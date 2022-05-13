04e1448582f76fd5da1e441e694b674dc8e8978b
Abhijeet Satpute <abhi111sat@gmail.com>
2022-05-13 14:11:44 +0530

    Styling AddUser Component
    Wrapping it with Card custom component and applying classes from css

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,3 +1,6 @@
+import Card from "../UI/Card"
+import classes from "./AddUser.module.css"
+
 const AddUser = () => {
     const submitHandler = (event) => {
         event.preventDefault();
@@ -5,6 +8,7 @@ const AddUser = () => {
     }
 
     return(
+        <Card className={classes.input}>
         <form onSubmit={submitHandler}>
             <label htmlFor="username">Username</label>
             <input id="username" type="text"/>
@@ -12,6 +16,7 @@ const AddUser = () => {
             <input id="age" type="number"/>
             <button type="submit">Add user</button>
         </form>
+        </Card>
     );
 }
 
