914e73528db75dbfc587ce77cd32acc1db371f30
Abhijeet Satpute <abhi111sat@gmail.com>
2022-05-13 14:51:25 +0530

    username & age  onChange state update

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,31 @@
+import { useState } from "react"
 import Card from "../UI/Card"
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button"
 
 const AddUser = () => {
+    const [enteredUsername, setEnteredUserName] = useState('')
+    const [enteredAge, setEnteredAge] = useState('')
     const submitHandler = (event) => {
         event.preventDefault();
-        console.log(event)
+        console.log(enteredUsername, enteredAge)
+    }
+
+    const usernameHandler = (event) => {
+        setEnteredUserName(event.target.value)
+    }
+
+    const ageHandler = (event) => {
+        setEnteredAge(event.target.value)
     }
 
     return(
         <Card className={classes.input}>
         <form onSubmit={submitHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text"/>
+            <input id="username" type="text" onChange={usernameHandler}/>
             <label htmlFor="age">Age</label>
-            <input id="age" type="number"/>
+            <input id="age" type="number" onChange={ageHandler}/>
             <Button type="submit">Add user</Button>
         </form>
         </Card>
