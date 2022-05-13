8b8ed52e3d602dc97040261826dfa120d31d1761
Abhijeet Satpute <abhi111sat@gmail.com>
2022-05-13 17:27:23 +0530

    Added ErrorModal reusable component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,6 +2,7 @@ import { useState } from "react"
 import Card from "../UI/Card"
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button"
+import ErrorModal from "../UI/ErrorModal"
 
 const AddUser = (props) => {
     const [enteredUsername, setEnteredUserName] = useState('')
@@ -10,18 +11,18 @@ const AddUser = (props) => {
     const [isAgeValid, setisAgeValid] = useState(true);
     const submitHandler = (event) => {
         event.preventDefault();
-        if(enteredUsername.trim()==="" && enteredAge.trim()===""){
+        if (enteredUsername.trim() === "" && enteredAge.trim() === "") {
             setisUsernameValid(false);
             setisAgeValid(false);
         }
-        if(enteredUsername.trim()===""){
+        if (enteredUsername.trim() === "") {
             setisUsernameValid(false);
             return;
-        } else if(enteredAge.trim()===""){
+        } else if (enteredAge.trim() === "") {
             setisAgeValid(false);
             return;
         }//here "+enteredAge" forces a string to be treated as a number
-          else if(+enteredAge < 0){
+        else if (+enteredAge < 0) {
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -31,33 +32,35 @@ const AddUser = (props) => {
 
     const usernameHandler = (event) => {
         setEnteredUserName(event.target.value)
-        if(event.target.value.trim()!==""){
+        if (event.target.value.trim() !== "") {
             setisUsernameValid(true);
-        } else if(event.target.value.trim()==="") {
+        } else if (event.target.value.trim() === "") {
             setisUsernameValid(false);
         }
     }
 
     const ageHandler = (event) => {
         setEnteredAge(event.target.value)
-        if(event.target.value>0){
+        if (event.target.value > 0) {
             setisAgeValid(true);
-        } else if(event.target.value<=0){
+        } else if (event.target.value <= 0) {
             setisAgeValid(false);
         }
     }
 
-    return(
-        <Card className={classes.input}>
-        <form onSubmit={submitHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" className={!isUsernameValid?classes.invalid:""} value={enteredUsername} onChange={usernameHandler}/>
-            <label htmlFor="age">Age</label>
-            <input id="age" type="number" className={!isAgeValid?classes.invalid:""} value={enteredAge} onChange={ageHandler}/>
-            <Button type="submit">Add user</Button>
-            {isAgeValid & isUsernameValid ? "":<p>Please enter correct details</p>}
-        </form>
-        </Card>
+    return (
+        <div>
+            <Card className={classes.input}>
+                <form onSubmit={submitHandler}>
+                    <label htmlFor="username">Username</label>
+                    <input id="username" type="text" className={!isUsernameValid ? classes.invalid : ""} value={enteredUsername} onChange={usernameHandler} />
+                    <label htmlFor="age">Age</label>
+                    <input id="age" type="number" className={!isAgeValid ? classes.invalid : ""} value={enteredAge} onChange={ageHandler} />
+                    <Button type="submit">Add user</Button>
+                    {isAgeValid & isUsernameValid ? "" : <p>Please enter correct details</p>}
+                </form>
+            </Card>
+        </div>
     );
 }
 
