9f66862d1f3b6c5db5e013b9edba6e4c6820a688
Abhijeet Satpute <abhi111sat@gmail.com>
2022-05-13 15:31:49 +0530

    Form validation with Dynamic Styling
    +Add invalid class if the username is empty when trimmed on the go
    +Add invalid class if the Age is below 0 on the go
    +Display text when input is invalid

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -6,29 +6,57 @@ import Button from "../UI/Button"
 const AddUser = () => {
     const [enteredUsername, setEnteredUserName] = useState('')
     const [enteredAge, setEnteredAge] = useState('')
+    const [isUsernameValid, setisUsernameValid] = useState(true);
+    const [isAgeValid, setisAgeValid] = useState(true);
     const submitHandler = (event) => {
         event.preventDefault();
+        if(enteredUsername.trim()==="" && enteredAge.trim()===""){
+            setisUsernameValid(false);
+            setisAgeValid(false);
+        }
+        if(enteredUsername.trim()===""){
+            setisUsernameValid(false);
+            return;
+        } else if(enteredAge.trim()===""){
+            setisAgeValid(false);
+            return;
+        }//here "+enteredAge" forces a string to be treated as a number
+          else if(+enteredAge < 0){
+            return;
+        }
         console.log(enteredUsername, enteredAge)
         setEnteredUserName('')
         setEnteredAge('')
     }
 
     const usernameHandler = (event) => {
+        console.log(event.target.value);
         setEnteredUserName(event.target.value)
+        if(event.target.value.trim()!==""){
+            setisUsernameValid(true);
+        } else if(event.target.value.trim()==="") {
+            setisUsernameValid(false);
+        }
     }
 
     const ageHandler = (event) => {
         setEnteredAge(event.target.value)
+        if(event.target.value>0){
+            setisAgeValid(true);
+        } else if(event.target.value<=0){
+            setisAgeValid(false);
+        }
     }
 
     return(
         <Card className={classes.input}>
         <form onSubmit={submitHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" value={enteredUsername} onChange={usernameHandler}/>
+            <input id="username" type="text" className={!isUsernameValid?classes.invalid:""} value={enteredUsername} onChange={usernameHandler}/>
             <label htmlFor="age">Age</label>
-            <input id="age" type="number" value={enteredAge} onChange={ageHandler}/>
+            <input id="age" type="number" className={!isAgeValid?classes.invalid:""} value={enteredAge} onChange={ageHandler}/>
             <Button type="submit">Add user</Button>
+            {isAgeValid & isUsernameValid ? "":<p>Please enter correct details</p>}
         </form>
         </Card>
     );
