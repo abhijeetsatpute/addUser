19ddf10414658b3ad2cb33321683ffe2e796f4a5
Abhijeet Satpute <abhi111sat@gmail.com>
2022-05-14 09:12:20 +0530

    Using Refs instead of States

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,64 +1,45 @@
-import { useState } from "react"
+import { useState , useRef } from "react"
 import Card from "../UI/Card"
 import classes from "./AddUser.module.css"
 import Button from "../UI/Button"
 import ErrorModal from "../UI/ErrorModal"
 
 const AddUser = (props) => {
-    const [enteredUsername, setEnteredUserName] = useState('')
-    const [enteredAge, setEnteredAge] = useState('')
-    const [isUsernameValid, setisUsernameValid] = useState(true);
-    const [isAgeValid, setisAgeValid] = useState(true);
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error,setError] = useState();
     const submitHandler = (event) => {
         event.preventDefault();
-        if (enteredUsername.trim() === "" && enteredAge.trim() === "") {
-            setisUsernameValid(false);
-            setisAgeValid(false);
+        const enteredName = nameInputRef.current.value;
+        const enteredUserAge = ageInputRef.current.value;
+        if (enteredName.trim() === "" && enteredUserAge.trim() === "") {
             setError({
                 "title" : "Invalid input",
                 "message": "Please enter a valid name and age (non-empty values)."
             })
             return;
         }
-        else if (enteredUsername.trim() === "") {
-            setisUsernameValid(false);
+        else if (enteredName.trim() === "") {
             setError({
                 "title" : "Invalid Username",
                 "message": "Please enter a valid username (non-empty values)."
             })
             return;
         }//here "+enteredAge" forces a string to be treated as a number
-        else if (+enteredAge < 0 || enteredAge.trim() === "") {
-            setisAgeValid(false);
+        else if (+enteredUserAge < 0 || enteredUserAge.trim() === "") {
             setError({
                 "title" : "Invalid Age",
                 "message": "Please enter a valid age (non-zero values)."
             })
             return;
         }
-        props.onAddUser(enteredUsername, enteredAge);
-        setEnteredUserName('')
-        setEnteredAge('')
+        props.onAddUser(enteredName, enteredUserAge);
+        //manipulating DOM via Refs
+        nameInputRef.current.value='';
+        ageInputRef.current.value='';
     }
 
-    const usernameHandler = (event) => {
-        setEnteredUserName(event.target.value)
-        if (event.target.value.trim() !== "") {
-            setisUsernameValid(true);
-        } else if (event.target.value.trim() === "") {
-            setisUsernameValid(false);
-        }
-    }
-
-    const ageHandler = (event) => {
-        setEnteredAge(event.target.value)
-        if (event.target.value > 0) {
-            setisAgeValid(true);
-        } else if (event.target.value <= 0) {
-            setisAgeValid(false);
-        }
-    }
 
     const errorHandler = () => {
         setError(null);
@@ -70,11 +51,18 @@ const AddUser = (props) => {
             <Card className={classes.input}>
                 <form onSubmit={submitHandler}>
                     <label htmlFor="username">Username</label>
-                    <input id="username" type="text" className={!isUsernameValid ? classes.invalid : ""} value={enteredUsername} onChange={usernameHandler} />
+                    <input 
+                        id="username" 
+                        type="text" 
+                        ref={nameInputRef}
+                    />
                     <label htmlFor="age">Age</label>
-                    <input id="age" type="number" className={!isAgeValid ? classes.invalid : ""} value={enteredAge} onChange={ageHandler} />
+                    <input 
+                        id="age" 
+                        type="number" 
+                        ref={ageInputRef}
+                    />
                     <Button type="submit">Add user</Button>
-                    {isAgeValid & isUsernameValid ? "" : <p>Please enter correct details</p>}
                 </form>
             </Card>
         </>
